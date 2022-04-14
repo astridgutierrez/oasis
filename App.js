1349725ef7dba8df2361b54551b72125f0f4b41b
astridgutierrez <75585497+astridgutierrez@users.noreply.github.com>
2022-04-14 16:29:45 -0500

    Update App.js
    functional app.js

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import HomeScreen from "C:/wamp64/www/oasis/Screens/HomeScreen.js";
+import LoadScreen from "C:/wamp64/www/oasis/Screens/LoadScreen.js";
+import PlantScreen from "C:/wamp64/www/oasis/Screens/PlantScreen.js";
+import StatusScreen from "C:/wamp64/www/oasis/Screens/StatusScreen.js";
+
+export default class App extends React.Component {
+    render() {
+
+        const Stack = createStackNavigator();
+        return (
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Homepage"
+                        component={HomeScreen}
+                        options={{title: 'Oasis'}}
+                    />
+                    <Stack.Screen name="Plant" component={PlantScreen}/>
+                    <Stack.Screen name="Status" component={StatusScreen}/>
+                    {/*<Stack.Screen name="Loading" component={LoadScreen}/>*/}
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        padding: 20,
+        borderWidth: 1,
+    }
 });
+
